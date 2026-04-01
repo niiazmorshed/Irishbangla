1ca27e426f6416906cebadb8e4c1cdfbe0531c30
Farheen Mahjarin Trisha <farheenmahjarin4@gmail.com>
2026-04-01 15:41:04 +0600

    about us section edited

--- irishbangla/src/components/Navbar.jsx
+++ irishbangla/src/components/Navbar.jsx
@@ -135,7 +135,7 @@ export default function Navbar() {
               setMenuOpen(false);
             }}
           >
-            Company
+            About us
           </li>
 
           {/* INFORMATION DROPDOWN */}
